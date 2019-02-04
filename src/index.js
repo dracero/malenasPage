c01dcab3d0166ffb247aaa1d4ccb728348c63dd6
dracero <dracero@users.noreply.github.com>
2019-02-04 15:45:05 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+require("../src/css/estilos.css");
+
+function App() {
+  return <div className="App" />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
